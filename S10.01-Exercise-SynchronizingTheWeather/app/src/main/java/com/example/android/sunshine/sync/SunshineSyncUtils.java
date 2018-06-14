7f143d09d452a4f0ade0be92be54ce65e393165b
Floyd Kots <floydkotohoyoh@gmail.com>
2018-06-14 12:27:02 +0300

    Synchronize the weather data using an intent service

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -1,3 +1,16 @@
-// TODO (9) Create a class called SunshineSyncUtils
-    //  TODO (10) Create a public static void method called startImmediateSync
-    //  TODO (11) Within that method, start the SunshineSyncIntentService
+package com.example.android.sunshine.sync;
+// COMPLETED TODO (9) Create a class called SunshineSyncUtils
+    // COMPLETED  TODO (10) Create a public static void method called startImmediateSync
+    // COMPLETED  TODO (11) Within that method, start the SunshineSyncIntentService
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.android.sunshine.sync.SunshineSyncIntentService;
+
+public class SunshineSyncUtils {
+    public static void startImmediateSync(final Context context) {
+        Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(intentToSyncImmediately);
+    }
+}
